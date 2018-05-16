d1469a995414621bacc885146b34bde896660de9
Muthu Kumar <muthukumar@thefeathers.in>
2018-05-16 18:47:37 +0530

    [misc] Added build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Browserifying..."
+npx browserify ./index.js -s isEq > ./es5/infinity.temp.js
+echo "Babelifying..."
+npx babel ./es5/infinity.temp.js --presets=env -o ./es5/infinity.js
+echo "Minifying output..."
+npx uglifyjs ./es5/infinity.js > ./es5/infinity.min.js
+echo "Cleaning up..."
+rm ./es5/infinity.temp.js
+echo "Done!"
